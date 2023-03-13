7b088c5d523be7d541b44462de4782a5e4873076
whkim1028 <whkim1028@naver.com>
2023-03-13 12:20:41 +0900

    1st

---
+++ src/main/java/mbti/mbticomm/MbticommApplication.java
@@ -0,0 +1,13 @@
+package mbti.mbticomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbticommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbticommApplication.class, args);
+    }
+
+}
